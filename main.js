ae17edaff1692b4e7c780cdd68aa848eda2c7c6c
JoaoUntura <joaovitorunturasteam@gmail.com>
2025-11-26 20:31:29 -0300

    Lista fix

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const {app, BrowserWindow} = require('electron')
 
 function createWindow (){
     const win = new BrowserWindow({
-        width:1920,
-        height:1080,
+        width:1366,
+        height:768,
         webPreferences: {
             nodeIntegration: true,      
             contextIsolation: false      
